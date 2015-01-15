8d0063a8f1520649a215bdaf54b5547d5985f361
Selby <david@selbys.org.uk>
2015-01-15 15:58:05 +0000

    Sentiment scorer almost basically working
    But not quite

--- scoreTweets.py
+++ scoreTweets.py
@@ -1,15 +1,50 @@
-import json
+import json, sys
+from string import punctuation
 
 '''Very very basic sentiment scorer that takes tweets and scores them according to an external corpus.
 Then returns time of tweet, hashtag(s) and score in a data structure.'''
 
-filename = 'exampledata.txt'
-f = open(filename, 'r')
-for line in f:
-	tweet = json.loads(line)
+def loadCorpus(file):
+    '''Opens a tab-separated file with 2 columns and returns a dictionary'''
+    with open(file, 'r') as f:
+        corpus = {line.split('\t')[0]: float(line.split('\t')[1]) for line in f}    
+    return corpus
 
-# def main():
-	# print 'Stuff.'
+def sentiment(text, corpus):
+    '''Sums sentiment score of words in a string that are found in a corpus'''
+    print text
+    score = 0.0
+    words = text.split(' ')
+    for word in words:
+        if word in corpus:
+            print '\t'+word, corpus[word]
+            score += corpus[word]
+    return score
 
-# if __name__ == '__main__':
-	# main()
+def loadTweets(file):
+    '''Loads JSON-formatted tweets (one tweet per line) into a list'''
+    with open(file, 'r') as f:
+        tweetList = [json.loads(line) for line in f]
+    print >> sys.stdout, len(tweetList), 'tweets loaded'
+    return tweetList
+
+def readTweet(tweet):
+    '''Return text, possibly with hashtags and symbols removed'''
+    textRaw = tweet['text'].lower()
+    for p in list(punctuation):
+        textCleaned = textRaw.replace(p,'')
+    return textRaw
+
+def main():
+    sentDict = loadCorpus('AFINN-111.txt')
+    tweets = loadTweets('exampledata.txt')
+    for tweet in tweets[10:50]:
+        tweetText = readTweet(tweet)
+        print 'CLEANED', '*'*30
+        print sentiment(tweetText, sentDict)
+        print 'UNCLEAN', '*'*30
+        print sentiment(tweet['text'], sentDict)
+        print '='*50
+
+if __name__ == '__main__':
+        main()
